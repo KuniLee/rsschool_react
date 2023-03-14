53780790e48b3f12fa283ce4d0ad5e8b2f37323f
Sitnikov <selnetys@gmail.com>
2023-03-14 23:01:33 +0500

    feat: add test for going about page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,7 @@ import { describe, it } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import App from '@/App'
 import { MemoryRouter, BrowserRouter } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
 
 describe('App', () => {
   it('full app rendering', () => {
@@ -19,5 +20,18 @@ describe('App', () => {
       </MemoryRouter>
     )
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Not Found')
+  })
+  it('test going to about page', async () => {
+    const user = userEvent.setup()
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    )
+    expect(screen.getByRole('link', { name: 'About Us' })).toHaveClass('text-gray-700')
+    await user.click(screen.getByRole('link', { name: 'About Us' }))
+    // eslint-disable-next-line testing-library/no-debugging-utils
+    expect(screen.getByRole('link', { name: 'About Us' })).toHaveClass('text-green-700')
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(/about/i)
   })
 })
