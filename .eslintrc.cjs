3926c86a93115b3b3cf87eeeedeec6bdd36284bb
Yuriy Sitnikov <selnetys@gmail.com>
2023-03-14 15:35:18 +0500

    feat: add test for routing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,57 +1,66 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "jest": true,
-        "node": true
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'jest': true,
+        'node': true
     },
-    "root": true,
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-        "plugin:react-hooks/recommended",
-        "plugin:prettier/recommended"
+    'root': true,
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+        'plugin:testing-library/react'
     ],
-    "overrides": [
+    'overrides': [{
+        files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+        extends: ['plugin:testing-library/react'],
+    }
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
         },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-        "prettier",
-        "react-hooks"
+    'plugins': [
+        'react',
+        '@typescript-eslint',
+        'prettier',
+        'react-hooks',
+        'testing-library'
     ],
-    "rules": {
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn",
-        "comma-dangle": ["error", "only-multiline"],
-        "react/prop-types": "off",
-        "react/display-name": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "prettier/prettier": ["error", { "endOfLine": "auto" }],
-        "@typescript-eslint/interface-name-prefix": "off",
-        "@typescript-eslint/ban-ts-comment": "error",
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-explicit-any": "error",
-        "@typescript-eslint/no-var-reqiures": "off",
-        "react/jsx-uses-react": "off",
-        "react/react-in-jsx-scope": "off"
+    'rules': {
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'comma-dangle': ['error', 'only-multiline'],
+        'react/prop-types': 'off',
+        'react/display-name': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        'prettier/prettier': ['error', {'endOfLine': 'auto'}],
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/ban-ts-comment': 'error',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/no-var-reqiures': 'off',
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'testing-library/await-async-query': 'error',
+        'testing-library/no-await-sync-query': 'error',
+        'testing-library/no-debugging-utils': 'off',
+        'testing-library/no-dom-import': 'off'
     },
-    "settings": {
-        "react": {
-            "pragma": "React",
-            "version": "detect"
+    'settings': {
+        'react': {
+            'pragma': 'React',
+            'version': 'detect'
         }
     }
 }
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,23 @@
 import { describe, it } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import App from '@/App'
+import { MemoryRouter, BrowserRouter } from 'react-router-dom'
 
 describe('App', () => {
-  it('Renders hello world', () => {
-    render(<App />)
-    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Hello')
+  it('full app rendering', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )
+    expect(screen.getByPlaceholderText('Search something..')).toBeInTheDocument()
+  })
+  it('renders not fount if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/this-path-is-incorrect']}>
+        <App />
+      </MemoryRouter>
+    )
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Not Found')
   })
 })
