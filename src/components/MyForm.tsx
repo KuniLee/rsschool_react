065566905ba14012feb6653eba84ad1c7f115ed1
Yuriy Sitnikov <selnetys@gmail.com>
2023-03-17 13:02:06 +0500

    feat: add MySelect component

--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -1,30 +1,34 @@
 import React, { ChangeEvent, Component, createRef } from 'react'
 
 import MyInput from '@components/UI/MyInput'
+import MySelect from '@components/UI/MySelect'
 
 class MyForm extends Component {
   input1 = createRef<HTMLInputElement>()
   input2 = createRef<HTMLInputElement>()
-  handler = (e: ChangeEvent<HTMLInputElement>) => {
+  input3 = createRef<HTMLSelectElement>()
+  handler = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     console.log(e.target.value)
   }
 
   render() {
     return (
       <form className="mt-1 max-w-[400px]">
-        <MyInput eMessage="cec" onChange={this.handler} placeholder="Insert title..." ref={this.input1} type="text">
+        <MyInput eMessage="cec" placeholder="Insert title..." ref={this.input1} type="text">
           Title
         </MyInput>
-        <MyInput eMessage="" onChange={this.handler} ref={this.input2} type="date">
+        <MyInput eMessage="" ref={this.input2} type="date">
           Date
         </MyInput>
-        {/*<select id="countries" className={classes.input}>*/}
-        {/*  <option selected>Choose a country</option>*/}
-        {/*  <option value="US">United States</option>*/}
-        {/*  <option value="CA">Canada</option>*/}
-        {/*  <option value="FR">France</option>*/}
-        {/*  <option value="DE">Germany</option>*/}
-        {/*</select>*/}
+        <MySelect
+          eMessage="f"
+          defaultName="Choose"
+          options={[
+            { name: '123', value: 'gg' },
+            { name: '234', value: 'second' },
+          ]}
+          ref={this.input3}
+        />
       </form>
     )
   }
---
+++ src/components/UI/MySelect.tsx
@@ -0,0 +1,45 @@
+import React, { Component, ComponentPropsWithoutRef } from 'react'
+
+type MySelectProps = ComponentPropsWithoutRef<'select'> & {
+  innerref: React.ForwardedRef<HTMLSelectElement>
+  options: Array<{ name: string; value: string }>
+  eMessage: string
+  defaultName: string
+}
+
+class MySelect extends Component<MySelectProps> {
+  id = `select-${this.props.defaultName}`
+
+  render() {
+    const { children, innerref, defaultName, eMessage, options, ...props } = this.props
+    return (
+      <div className="relative pb-5">
+        {children && (
+          <label htmlFor={this.id} className="block mb-1 text-sm font-medium text-gray-900">
+            {children}
+          </label>
+        )}
+        <select
+          defaultValue="default"
+          id={this.id}
+          className={`border text-sm rounded-lg block w-full p-2.5
+          ${eMessage ? 'border-red-500 text-red-900 bg-red-50' : 'bg-gray-50 text-gray-900 focus:ring-green-500 focus:border-green-500 border-gray-300'}`}
+          ref={innerref}
+          {...props}
+        >
+          <option value="default" disabled>
+            {defaultName}
+          </option>
+          {options.map((el) => (
+            <option key={el.value} value={el.value}>
+              {el.name}
+            </option>
+          ))}
+        </select>
+        {eMessage && <p className="absolute bottom-0 text-sm text-red-600 dark:text-red-500">{eMessage}</p>}
+      </div>
+    )
+  }
+}
+
+export default React.forwardRef<HTMLSelectElement, Omit<MySelectProps, 'innerref'>>((props, ref) => <MySelect innerref={ref} {...props} />)
