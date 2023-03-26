4dd3da4b9336385934d5a29a8aa5fbc429998c33
Sitnikov <selnetys@gmail.com>
2023-03-26 12:01:51 +0500

    refactor: change Search.tsx to function component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,50 +1,34 @@
-import React, { ChangeEvent, Component } from 'react'
+import React, { FC, useEffect, useRef } from 'react'
 import { ReactComponent as Logo } from '@/assets/icons/search.svg'
-
-type SearchState = {
-  input: string
-}
 
 type SearchProps = Record<string, never>
 
-class Search extends Component<SearchProps, SearchState> {
-  constructor(props: SearchProps) {
-    super(props)
-    this.state = {
-      input: localStorage.searchInput || '',
+const Search: FC<SearchProps> = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    const input = inputRef.current
+    if (input) input.value = localStorage.searchInput || ''
+    return () => {
+      localStorage.searchInput = input?.value || ''
     }
-  }
+  }, [])
 
-  inputHandle = (e: ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { value: input },
-    } = e
-    this.setState({ input })
-  }
-
-  componentWillUnmount() {
-    localStorage.searchInput = this.state.input
-  }
-
-  render() {
-    const { input } = this.state
-    return (
-      <div className="max-w-md mx-auto">
-        <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
-          <div className="grid place-items-center h-full w-12 text-gray-300">
-            <Logo className="w-6 h-6"></Logo>
-          </div>
-          <input
-            value={input}
-            onChange={this.inputHandle}
-            className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
-            type="text"
-            placeholder="Search something.."
-          />
+  return (
+    <div className="max-w-md mx-auto">
+      <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+        <div className="grid place-items-center h-full w-12 text-gray-300">
+          <Logo className="w-6 h-6"></Logo>
         </div>
+        <input
+          ref={inputRef}
+          className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+          type="text"
+          placeholder="Search something.."
+        />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Search
