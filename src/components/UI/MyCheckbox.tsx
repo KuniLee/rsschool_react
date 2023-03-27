ad1a23fcd3911fd32bf53c63acdb82056c187eb1
Yuriy Sitnikov <selnetys@gmail.com>
2023-03-27 15:28:26 +0500

    refactor: change MyCheckbox.tsx to FC

--- src/components/UI/MyCheckbox.tsx
+++ src/components/UI/MyCheckbox.tsx
@@ -1,31 +1,23 @@
-import React, { Component, ComponentPropsWithoutRef } from 'react'
+import React, { ComponentPropsWithoutRef, forwardRef } from 'react'
 
-type MyCheckboxProps = ComponentPropsWithoutRef<'input'> & {
-  innerref: React.ForwardedRef<HTMLInputElement>
-}
+type MyCheckboxProps = ComponentPropsWithoutRef<'input'>
 
-class MyCheckbox extends Component<MyCheckboxProps> {
-  id = `${this.props.type}-${this.props.children}`
+export const MyCheckbox = forwardRef<HTMLInputElement, MyCheckboxProps>(({ children, ...props }, ref) => {
+  const id = `${props.type}-${children}`
+  return (
+    <div className="flex items-center relative mb-1">
+      <input
+        {...props}
+        id={id}
+        type="checkbox"
+        ref={ref}
+        className="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded"
+      />
+      <label htmlFor={id} className="ml-2 text-sm font-medium text-gray-900">
+        {children}
+      </label>
+    </div>
+  )
+})
 
-  render() {
-    const { children, innerref, ...props } = this.props
-    return (
-      <div className="flex items-center relative mb-1">
-        <input
-          id={this.id}
-          type="checkbox"
-          ref={innerref}
-          {...props}
-          className="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded"
-        />
-        <label htmlFor={this.id} className="ml-2 text-sm font-medium text-gray-900">
-          {children}
-        </label>
-      </div>
-    )
-  }
-}
-
-export default React.forwardRef<HTMLInputElement, Omit<MyCheckboxProps, 'innerref'>>((props, ref) => (
-  <MyCheckbox innerref={ref} {...props} />
-))
+export default MyCheckbox
