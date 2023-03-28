5ab8ed17933345ac402a52b05d6793906de8f4c4
Yuriy Sitnikov <selnetys@gmail.com>
2023-03-28 11:13:20 +0500

    refactor: change Popup.tsx to FC

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,4 +1,4 @@
-import React, { Component, createRef, MouseEventHandler } from 'react'
+import React, { FC, MouseEventHandler, useRef } from 'react'
 import { CSSTransition } from 'react-transition-group'
 import MyButton from '@components/UI/MyButton'
 
@@ -8,34 +8,30 @@ type PopupProps = {
   onOk: MouseEventHandler
 }
 
-class Popup extends Component<PopupProps> {
-  nodeRef = createRef<HTMLDivElement>()
-
-  render() {
-    const { open, msg, onOk } = this.props
-    return (
-      <CSSTransition
-        timeout={500}
-        nodeRef={this.nodeRef}
-        in={open}
-        classNames={{ enterActive: '!z-50 opacity-100', enterDone: '!z-50 opacity-100', exitActive: '!z-50' }}>
-        <div
-          ref={this.nodeRef}
-          className={`transition-opacity duration-500 flex -z-10 fixed opacity-0 items-center justify-center top-0 left-0 right-0 w-screen h-screen backdrop-blur-sm`}>
-          <div className="bg-green-100 p-4 rounded-lg border-4 border-green-600 max-w-[300px] flex flex-col justify-center">
-            <p className="mb-2">{msg}</p>
-            <MyButton
-              onClick={(event) => {
-                event.preventDefault()
-                onOk(event)
-              }}>
-              Ок
-            </MyButton>
-          </div>
+const Popup: FC<PopupProps> = ({ msg, onOk, open }) => {
+  const nodeRef = useRef<HTMLDivElement>(null)
+  return (
+    <CSSTransition
+      timeout={500}
+      nodeRef={nodeRef}
+      in={open}
+      classNames={{ enterActive: '!z-50 opacity-100', enterDone: '!z-50 opacity-100', exitActive: '!z-50' }}>
+      <div
+        ref={nodeRef}
+        className={`transition-opacity duration-500 flex -z-10 fixed opacity-0 items-center justify-center top-0 left-0 right-0 w-screen h-screen backdrop-blur-sm`}>
+        <div className="bg-green-100 p-4 rounded-lg border-4 border-green-600 max-w-[300px] flex flex-col justify-center">
+          <p className="mb-2">{msg}</p>
+          <MyButton
+            onClick={(event) => {
+              event.preventDefault()
+              onOk(event)
+            }}>
+            Ок
+          </MyButton>
         </div>
-      </CSSTransition>
-    )
-  }
+      </div>
+    </CSSTransition>
+  )
 }
 
 export default Popup
